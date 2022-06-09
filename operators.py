081e0e1c7a07e1997085b54542afc8efd744330c
Alishadixit <91393612+Alishadixit@users.noreply.github.com>
2022-06-10 00:59:48 +0530

    Initial commit

---
+++ operators.py
@@ -0,0 +1,37 @@
+a=10
+b=6
+#add two numbers
+c=a+b
+print(c)
+#subbtract two numbers
+c=a-b
+print(c)
+#divide two number
+c=a/b
+print(c)
+c=a//b
+print(c)
+c=a%b
+print(c)
+#use exponent function
+print('a**b',a**b)
+'''COMPARISION OPERATORS'''
+print('a<b',a<b)
+print('a>b',a>b)
+print('a==b',a==b)
+print('a!=b',a!=b)
+print('a<=b',a<=b)
+print('a>=b',a>=b)
+'''LOGICAL OPERATORS'''
+print('a and b',a and b)
+print('a or b',a or b)
+print('not a',not a)
+'''BITWISE OPERATORS'''
+print('a & b',a & b)
+print('a | b',a | b)
+# 9=1001;6=0110;9+6=1111 bitwise or means any 1 0 is 1 ;bitwise and means if both 1 than 1.
+print('~a',~a)
+print('a^b',a^b)
+print('a<<b',a <<b)
+print('a>>b',a >>b)
+

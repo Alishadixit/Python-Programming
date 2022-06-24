c68b8e1df0d6cb6fd3c280c658c30bc8af201777
Alishadixit <91393612+Alishadixit@users.noreply.github.com>
2022-06-24 23:04:57 +0530

    Sum of natural no.

---
+++ programs.py
@@ -0,0 +1,8 @@
+# sum  of no from 1 to 10
+#for i in range(1, 10)
+l = 100;
+a= 1;
+n = 100;
+Sn = {n // 2 * (a + l)}
+print("sum is",Sn)
+#Here we have use arithmetic progression formula  where l = last term ,d= difference,sn=sum,a=first term

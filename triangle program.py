680a94ebf6b4409a96600635b4d229195bd91089
Alishadixit <91393612+Alishadixit@users.noreply.github.com>
2022-06-26 13:30:41 +0530

    WAP to find Area of Triangle.

--- triangle program.py
+++ triangle program.py
@@ -1,10 +1,12 @@
-#sum of triangle
-from typing import Any
+#AREA OF TRIANGLE
 
-a=int(input("Enter Your first Side"))
-b=int(input("Enter Your second Side"))
-c=int(input("Enter your third side"))
-sum  =(a+b+c/2)
-area: float | Any=(sum(sum-a)*(sum-b)*(sum-c))*0.5
-print(area)
+
+a=float(input("Enter Your first Side"))
+b=float(input("Enter Your second Side"))
+c=float(input("Enter your third side"))
+sum  =(a+b+c) / 2
+area = (sum*(sum-a)*(sum-b)*(sum-c))**0.5
+print('The area of the triangle is %0.2f' %area)
+
+
 

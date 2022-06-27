2730a874a107de9f1591b2194151316f4366e897
Alishadixit <91393612+Alishadixit@users.noreply.github.com>
2022-06-27 21:06:53 +0530

    WAP to find roots using quadratic formula.

---
+++ quadratic equation.py
@@ -0,0 +1,16 @@
+import cmath
+a=int(input("Enter value of a :"))
+b=int(input("Enter the value of b :"))
+c=int(input("Enter the value of c "))
+disc=(b**2) - (4*a*c)
+sol1 = (-b-cmath.sqrt(disc))/(2*a)
+sol2 = (-b+cmath.sqrt(disc)/(2*a))
+if disc>0:
+    print("roots are real and different")
+    print(sol1,sol2)
+elif disc==0:
+    print ("both roots are equal")
+    print(sol1,sol2)
+else:
+    print("roots are complex")
+    print(sol1,sol2,sep="and")
